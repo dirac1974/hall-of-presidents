2a65f96c2854951931114c813a608a6b16e87a98
dirac1974 <davidstucke@gmail.com>
2026-08-30 13:14:49 -0700

    Point #25 at Mount McKinley picture

--- images.js
+++ images.js
@@ -1,4 +1,4 @@
-const IMG_VER = "8303";
+const IMG_VER = "8304";
 const IMG_IDS = {
   1:"1TD9UsI14L1fsPHnnCsqG9HHdrLD1IfDH",
   2:"1mea_p_THjTZrBn2qRFUuWs15U-Kw8a7P",
@@ -24,7 +24,7 @@ const IMG_IDS = {
   22:"1RufaeaLDyKSS_f_6YoDhuTDdXKdeoxIx",
   23:"1sfcSrEFhMnr0xM5N5VQ91jyQy93FeVH1",
   24:"1k-0fTgeZiys0SBsrzHyaWaa_jcZsh3j0",
-  25:"1N1LFVmJ3QHaRXiMcj4wsf2yN16uTK7MW",
+  25:"118JM2rabUzztr8tGCJlaYAmMHmtnvf1L",
   26:"1_bfbpEapK9T_8qzzX3bXLW1LcpBMItxd",
   27:"1kUGAB3K3_VAjaR3CTOgeao7wOrlOXfJO",
   28:"1GqXIgKqBzdoZ18qNW2OtjF-qlZn1d5UR",
