35ad99c36678684f08f8f5c6cca163c7555858da
dirac1974 <davidstucke@gmail.com>
2026-08-30 13:28:30 -0700

    Taft cue: Will stuck in taffy shaped like 27

--- presidents.js
+++ presidents.js
@@ -25,7 +25,7 @@ const PRESIDENTS = [
   { n:24, name:"Grover Cleveland", short:"Cleveland 2", mnemonic:"The same cleaver comes back for round two!", emoji:"🌽" },
   { n:25, name:"William McKinley", short:"McKinley", mnemonic:"McDonald the KING at the KINLEY gate with 25 burgers.", emoji:"🍔" },
   { n:26, name:"Theodore Roosevelt", short:"T. Roosevelt", mnemonic:"Teddy on a horse wearing a belt of 26 ROSES.", emoji:"🌹" },
-  { n:27, name:"William Howard Taft", short:"Taft", mnemonic:"Stuck in sticky TAFFY!", emoji:"🍬" },
+  { n:27, name:"William Howard Taft", short:"Taft", mnemonic:"WILL stuck in sticky TAFFY shaped like 27.", emoji:"🍬" },
   { n:28, name:"Woodrow Wilson", short:"Wilson", mnemonic:"A will (document) coming out of a coffin like a zombie.", emoji:"📜" },
   { n:29, name:"Warren G. Harding", short:"Harding", mnemonic:"A giant metal HARD man standing in the street.", emoji:"🤖" },
   { n:30, name:"Calvin Coolidge", short:"Coolidge", mnemonic:"Standing in a big COOLER keeping cool.", emoji:"🧊" },
